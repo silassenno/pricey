de98bd65c01fa51a21e5c730efd679e62a177fc8
Silas Senno <39792037+silassenno@users.noreply.github.com>
2024-01-16 10:34:25 +0100

    addexpense addition

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from flask import render_template
 from sqlite3 import connect
+from flask import request
 
 app = Flask("pricey")
-app.connect=connect("pricey.db")
+app.connection=connect("pricey.db")
+@app.route("/expense/add", methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        cursor=app.connection.cursor()
+        cursor.execute("INSERT INTO expense")
+        #insert into database
+        pass
+    return render_template('addexpense.html')
 @app.route('/')
 def home():
     return render_template('index.html')
